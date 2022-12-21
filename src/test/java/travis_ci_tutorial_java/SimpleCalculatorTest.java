69e3e2b33a305aa0c8f0e4a70799ed7004c763e8
aredi1 <115046689+aredi1@users.noreply.github.com>
2022-12-20 23:01:08 -0500

    Update SimpleCalculatorTest.java

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -13,6 +13,6 @@ public class SimpleCalculatorTest {
 		assertEquals(calc.minus(5, 3), 2);
 		assertEquals(calc.multiply(1, 1), 1);
 		assertEquals(calc.divide(1, 1), 1);
-		assertEquals(calc.mod(1, 10), 1);
+		assertEquals(calc.mod(1, 1), 0);
 	}
 }
